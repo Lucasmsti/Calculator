abace93a73637676aeb48e199d9a4cceef23a643
Lucasmsti <lucasmsantos.ti@gmail.com>
2023-09-08 16:22:35 -0300

    feat: add updade scream and click button +

--- script.js
+++ script.js
@@ -11,13 +11,46 @@ class Calculator {
 
     // adicionando dígito na tela
     add_digit(digit) {
+        // checando se a operação atual ja tem um ponto
+        if(digit === "." && this.current_operation_text.innerText.includes(".")) {
+            return;
+        }
+
         this.current_operation = digit;
         this.update_scream();
     }
 
+    // processando todas as operações
+    process_operation(operation) {
+        // pegandoo valor atual e anterior
+        let operation_value;
+        let previous = +this.prev_operation_text.innerText;
+        let current = +this.current_operation_text.innerText;
+
+        switch(operation) {
+            case "+":
+                operation_value = previous + current;
+                this,this.update_scream(operation_value, operation, current, previous)
+                break;
+            default:
+                return;
+        }
+    }
+
     // mudando valores da tela
-    update_scream() {
-        this.current_operation_text.innerText += this.current_operation;
+    update_scream(operation_value = null, operation = null, current = null, previous = null) {
+        if (operation_value === null) {
+            this.current_operation_text.innerText += this.current_operation;
+        } else {
+            // checagem se o valur é zero, se for entao add o valor atual
+            if (previous === 0) {
+                operation_value = current
+            }
+
+            // adiciona o valor atual para previous
+            this.prev_operation_text.innerText = `${operation_value} ${operation}`;
+            this.current_operation_text.innerText = "";
+        }
     }
 }
 
@@ -30,7 +63,7 @@ btns.forEach((buttons) => {
         if(+value >= 0 || value === ".") {
             calc.add_digit(value)
         } else {
-            console.log("Op: " + value)
+            calc.process_operation(value)
         }
     })
 });
